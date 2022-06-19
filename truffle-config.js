4cffed255507b46832c76f7e294e6f9a915f12bd
91savage <ekdma1403@gmail.com>
2022-06-19 23:24:10 +0900

    0619

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const NETWORK_ID = "1001";
 const GASLIMIT = "20000000";
 const URL = `https://api.baobab.klaytn.net:8651`;
 const PRIVATE_KEY =
-  "";
+  "0xd25293075580320fdbb666d5ba65f5a2870cef3055cfd8eb4076f4b95766e1d7";
 
 module.exports = {
   networks: {
